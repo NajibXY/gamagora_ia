00198ce96c5a897d7743ce421182e64127c40c02
NajibXY <naj.elkhadir@gmail.com>
2024-10-29 02:15:29 +0100

    fix djikstra to find best solution amongst best paths to goals

--- godot/projects/ia_jv_1/scenes/scripts/utils/djikstra.gd
+++ godot/projects/ia_jv_1/scenes/scripts/utils/djikstra.gd
@@ -26,19 +26,23 @@ class Tuple:
 
 # Dijkstra's algorithm implementation with multiple goals and path recalling
 func dijkstra_multi_goal(graph: Dictionary, start: String, goals: Array) -> Dictionary:
+	# Return dictionary
+	var return_dict = {}
 	# Priority queue
 	var queue = []
 	# Initialize the queue with the start and its 0 cost
 	queue.append({"cost": 0, "node": start})
 
 	# Distance dictionary to track the shortest path costs
+	# Initialize all distances to infinity
 	var distances = {}
 	for key in graph.keys():
 		distances[key] = INF
 	distances[start] = 0
 
 	# Visited notes 
-	var visited = {}
+	# var visited = {}
+
 	# Previous node in the path
 	var previous = {}
 
@@ -49,27 +53,41 @@ func dijkstra_multi_goal(graph: Dictionary, start: String, goals: Array) -> Dict
 		var current_node = queue[0]["node"]
 		queue.remove_at(0)
 
-		# If we reach a goal node, reconstruct the path
-		################## Djisktra is a greedy algorithm, so the first time we reach a goal, it is the shortest path for its non heuristic based approach... ##################
-		# It can have BAD consequences on the decision making of the AI, but it is a good start
-		if current_node in goals:
-			return {
-				"distance": current_cost,
-				"path": reconstruct_path(previous, start, current_node)
-			}
+		# if visited.has(current_node):
+		# 	continue
 
-		if visited.has(current_node):
-			continue
-
-		visited[current_node] = true
+		# visited[current_node] = true
 		for neighbor in graph[current_node].keys():
-			if not visited.has(neighbor):
-				var new_cost = current_cost + graph[current_node][neighbor]
-				if new_cost < distances[neighbor]:
-					distances[neighbor] = new_cost
-					# Track the previous node for the path
-					previous[neighbor] = current_node 
-					queue.append({"cost": new_cost, "node": neighbor})  
+			# if not visited.has(neighbor):
+			var new_cost = current_cost + graph[current_node][neighbor]
+			if new_cost < distances[neighbor]:
+				distances[neighbor] = new_cost
+				# Track the previous node for the path
+				previous[neighbor] = current_node 
+				queue.append({"cost": new_cost, "node": neighbor})  
+				
+		################## Djisktra is a greedy algorithm, it is the shortest path to a goal with a non heuristic based approach... ##################
+		# It can have BAD consequences on the decision making and the complexity calcualtion of the path
+		if current_node in goals:
+			if return_dict.has("distance"):
+				print("Old shortest path cost: ", return_dict["distance"])
+				if current_cost < return_dict["distance"]:
+					print("New shortest path found cost: ", current_cost)
+					return_dict = {
+						"distance": current_cost,
+						"path": reconstruct_path(previous, start, current_node)
+					}
+			else:
+				print("First shortest path found cost: ", current_cost)
+				return_dict = {
+					"distance": current_cost,
+					"path": reconstruct_path(previous, start, current_node)
+				}
+
+	# Return the shortest path cost and the path
+	if return_dict.has("distance"):
+		return return_dict
+
 	# Return INF for infinity if no goals are reachable
 	return {"distance": INF, "path": []} 
 
